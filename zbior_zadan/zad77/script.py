c3ada048641d625ea2b7c45d197dbdaf6e862407
Pepe <stockipiotrek1@gmail.com>
2024-03-06 15:03:04 +0100

    zmiany widzowie zmiany
    informatyka -> zbior_zadan
    +matury

---
+++ zbior_zadan/zad77/script.py
@@ -0,0 +1,6 @@
+file1 = open("./77/dokad.txt", "r")
+file2 = open("./77/szyfr.txt", "r")
+
+letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'] 
+
+def cipher()
